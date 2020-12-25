114f38cddd3f6488b11491e2c6601ea32921db47
Aleksei Potapov <potapov.alex@hotmail.com>
2020-12-25 14:00:40 +0300

    sorts 04 unknown

--- algoritmization/sorts/SortTask04.java
+++ algoritmization/sorts/SortTask04.java
@@ -2,8 +2,32 @@ package by.epam.java.algoritmization.sorts;
 
 /**
  * @author potap; Дана последовательность a1<=a2<=...<=an.
- * Написать алгоритм сортировки выбором
+ * Переставить числа в порядке возрастания: сравнить i и i+1, посчитать перестановки.
  */
 
 public class SortTask04 {
+    public static void main(String[] args) {
+
+        int numericTemp;
+        int counter;
+        counter = 0;
+
+        int[] arrayA;
+        arrayA = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int i = 0; i < arrayA.length-1; i++) {
+            if (arrayA[i] > arrayA[i + 1]) {
+                numericTemp = arrayA[i];
+                arrayA[i] = arrayA[i + 1];
+                arrayA[i + 1] = numericTemp;
+                counter++;
+            }
+        }
+
+        System.out.println("\n Итоговый массив : " );
+        for (int i = 0; i < arrayA.length; i++) {
+            System.out.print(arrayA[i] + "\t");
+        }
+        System.out.println("\nВыполнено перестановок: "+counter);
+    }
 }
