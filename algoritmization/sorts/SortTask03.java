6977c181100800412eb1dbf5c4f04c2e1fb31240
Aleksei Potapov <potapov.alex@hotmail.com>
2020-12-25 13:21:41 +0300

    sort task 3 ready

--- algoritmization/sorts/SortTask03.java
+++ algoritmization/sorts/SortTask03.java
@@ -12,11 +12,42 @@ public class SortTask03 {
 
         int[] arrayA;
         arrayA = new int[10];
+        System.out.println("Исходный массив\t");
         for (int i = 0; i < arrayA.length; i++) {
             Random rnd = new Random();
-            arrayA[i] = rnd.nextInt(10);
-            System.out.print(arrayA[i] + " ");
+            arrayA[i] = rnd.nextInt(15);
+            System.out.print(arrayA[i] + "\t");
         }
-        
+
+        int numericTemp;
+        int numericMax;
+        int counter;
+        counter = 0;
+
+        for (int i = 0; i < arrayA.length; i++) {
+            numericTemp = i;
+            numericMax = arrayA[i];
+
+            for (int j = i + 1; j < arrayA.length; j++) {
+                if (arrayA[j] > numericMax) {
+                    numericTemp = j;
+                    numericMax = arrayA[j];
+                }
+            }
+            arrayA[numericTemp] = arrayA[i];
+            arrayA[i] = numericMax;
+            counter++;
+
+            System.out.println("\n Массив после переставновки " + counter + "\t");
+            for (int k = 0; k < arrayA.length; k++) {
+                System.out.print(arrayA[k] + "\t");
+            }
+        }
+
+        System.out.println("\n Итоговый массив ");
+        for (int i = 0; i < arrayA.length; i++) {
+            System.out.print(arrayA[i] + "\t");
+        }
+        System.out.println("\nВыполнено перестановок : "+counter);
     }
 }
