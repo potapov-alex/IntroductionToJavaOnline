f9a30e4bb2f0d4c79222b00f91785516d9b20838
Aleksei Potapov <potapov.alex@hotmail.com>
2021-01-15 20:40:38 +0300

    sort task 13 done

--- algoritmization/decomposition/DecoTask13.java
+++ algoritmization/decomposition/DecoTask13.java
@@ -10,6 +10,8 @@ import java.util.Scanner;
  */
 
 public class DecoTask13 {
+    static int numberN = consoleRetrieves("Введите число N");
+    static int doubleN = numberN * 2;
 
     public static int consoleRetrieves(String message) {
         @SuppressWarnings("resource")
@@ -27,9 +29,17 @@ public class DecoTask13 {
         return number;
     }
 
+    public static void twinNumbers(int number) {
+        int[] arrayList = new int[2];
+        for (int i = 0; i < arrayList.length-1; i++) {
+            arrayList[i] = number;
+            System.out.printf("число %d с бизнецом %d\n", arrayList[i], arrayList[i] + 2);
+        }
+    }
+
     public static void main(String[] args) {
-
-
-
+        for (int i = numberN; i < doubleN - 1; i++) {
+            twinNumbers(i);
+        }
     }
 }
